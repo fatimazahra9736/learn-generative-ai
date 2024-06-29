ca4535533d561a9e68bff28a92f9dd5f3a306991
poonyfatima <fatimazahra9736@gmail.com>
2024-06-29 06:13:41 -0700

    chapter-8 with def

--- 02_python_crash_course/python_crash_course_book_code_with_typing/exercises/my_script.py
+++
@@ -1,6 +0,0 @@
-# import encodings
-# for table in range(11, 21):
-#     print(f"==== {table} ====")
-#     for number in range(1, 11):
-#         print(f"{table} x {number} = {table*number}")
-# print(encodings)
